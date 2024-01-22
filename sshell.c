d54f088071306e26b812144c0bb3544c28ab86d3
fongbrandon1928 <146702122+fongbrandon1928@users.noreply.github.com>
2024-01-21 17:18:31 -0800

    Update sshell.c
    Can now echo strings with multiple words with spaces in between them

--- sshell.c
+++ sshell.c
@@ -7,12 +7,13 @@
 
 #define CMDLINE_MAX 512
 
-int main(void){
+int main(void)
+{
     char cmd[CMDLINE_MAX];
 
-    while (1) {
+    while (1)
+    {
         char *nl;
-        char fullCommand[CMDLINE_MAX];
 
         /* Print prompt */
         printf("sshell@ucd$ ");
@@ -20,83 +21,119 @@ int main(void){
 
         /* Get command line */
         fgets(cmd, CMDLINE_MAX, stdin);
-        
-        strcpy(fullCommand, cmd);
 
         /* Print command line if stdin is not provided by terminal */
-        if (!isatty(STDIN_FILENO)) {
+        if (!isatty(STDIN_FILENO))
+        {
             printf("%s", cmd);
             fflush(stdout);
         }
+        
         int cmdNum = 0;
-        char* cmdStr[64];
+        char *cmdStr[64];
         int redirect = 0;
         char *filename = NULL;
 
-        // split cmd with space
-        char *token = strtok(cmd, " \n"); 
-        // cmdStr gets these commands in a char* list
-        // cmdNum gets the number of elements
-        while (token != NULL) {
-            if (strcmp(token, ">") == 0) {
+        /* File redirect */
+        char *token = strtok(cmd, " \n");
+        while (token != NULL)
+        {
+            if (strcmp(token, ">") == 0)
+            {
                 redirect = 1;
                 token = strtok(NULL, " \n");
-                if (token != NULL) {
-                    filename = token;
-                }
+                filename = token;
                 break;
-            } else {
+            }
+            else
+            {
                 cmdStr[cmdNum++] = token;
-                token = strtok(NULL, " \n");
-            } 
+            }
+            token = strtok(NULL, " \n");
         }
+        cmdStr[cmdNum] = NULL;
+        
         /* Remove trailing newline from command line */
         nl = strchr(cmdStr[0], '\n');
         if (nl)
             *nl = '\0';
 
-        for (int i = 0; i < cmdNum; i++) {
-            if (strcmp(cmdStr[i], ">") == 0) {
+        for (int i = 0; i < cmdNum; i++)
+        {
+            if (strcmp(cmdStr[i], ">") == 0)
+            {
                 redirect = 1;
                 cmdStr[i] = NULL;
-                if (i + 1 < cmdNum) {
-                    filename = cmdStr[i+1];
+                if (i + 1 < cmdNum)
+                {
+                    filename = cmdStr[i + 1];
                 }
                 break;
             }
         }
+        cmdStr[cmdNum] = NULL;
 
-        if (!strcmp(cmdStr[0], "cd")) {
-            if (cmdStr[1] != NULL) {
-                if (chdir(cmdStr[1]) != 0) {
+	/* Complete message to print full command */
+        char fullCmd[CMDLINE_MAX] = "";
+        for (int i = 0; i < cmdNum; i++)
+        {
+            strcat(fullCmd, cmdStr[i]);
+            if (i < cmdNum - 1)
+            {
+                strcat(fullCmd, " ");
+            }
+        }
+        if (redirect && filename)
+        {
+            strcat(fullCmd, " > ");
+            strcat(fullCmd, filename);
+        }
+
+        if (!strcmp(cmdStr[0], "cd"))
+        {
+            if (cmdStr[1] != NULL)
+            {
+                if (chdir(cmdStr[1]) != 0)
+                {
                     perror("cd");
                 }
-            } 
-            else {
+            }
+            else
+            {
                 fprintf(stderr, "No path specified for cd\n");
             }
-        }       
+        }
 
         /* Builtin command */
-        else if (!strcmp(cmdStr[0], "exit")) {
+        else if (!strcmp(cmdStr[0], "exit"))
+        {
             fprintf(stderr, "Bye...\n");
             break;
         }
-        else if (!strcmp(cmdStr[0], "pwd")) {
-            char cwd[512];
-            if (getcwd(cwd, sizeof(cwd)) != NULL) {
+        else if (!strcmp(cmdStr[0], "pwd"))
+        {
+            char cwd[CMDLINE_MAX];
+            if (getcwd(cwd, sizeof(cwd)) != NULL)
+            {
                 printf("%s\n", cwd); // print dic
-            } else {
+            }
+            else
+            {
                 perror("getcwd"); // error
             }
         }
-        else {
+
+        else
+        {
             __pid_t pid;
             pid = fork();
-            if (pid == 0) {
-                if (redirect && filename) {
+            if (pid == 0)
+            {
+                if (redirect && filename)
+                {
                     int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-                    if (fd < 0) {
+                    if (fd < 0)
+                    {
                         perror("open");
                         exit(EXIT_FAILURE);
                     }
@@ -107,12 +144,14 @@ int main(void){
                 perror("execvp");
                 exit(EXIT_FAILURE);
             }
-            else if (pid > 0) {
+            else if (pid > 0)
+            {
                 int status;
                 waitpid(pid, &status, 0);
-                printf("+ complete '%s' [%d]\n", cmdStr[0], WEXITSTATUS(status));
+                printf("+ complete '%s' [%d]\n", fullCmd, WEXITSTATUS(status));
             }
-            else {
+            else
+            {
                 perror("fork");
                 exit(1);
             }
